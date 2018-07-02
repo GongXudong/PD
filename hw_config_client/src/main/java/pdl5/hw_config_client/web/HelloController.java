6ca9695856ce59054ae7210c8c200b406771138b
gongxudong <gongxudong_cs@aliyun.com>
2018-07-02 18:02:35 +0800

    config client

---
+++ hw_config_client/src/main/java/pdl5/hw_config_client/web/HelloController.java
@@ -0,0 +1,16 @@
+package pdl5.hw_config_client.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from(){
+        return this.hello;
+    }
+}
