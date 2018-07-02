f68dacaec39e4e17f72086ec645a177083985739
gongxudong <gongxudong_cs@aliyun.com>
2018-07-02 10:48:41 +0800

    hystrix and configure server

---
+++ hw_eureka_consumer/src/main/java/pdl5/hw_eureka_consumer/remote/HelloRemoteHystrix.java
@@ -0,0 +1,13 @@
+package pdl5.hw_eureka_consumer.remote;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+public class HelloRemoteHystrix implements HelloRemote {
+
+    @Override
+    public String hello(@RequestParam(value="name") String name) {
+        return "hello" + name + ", this message send failed.";
+    }
+}
